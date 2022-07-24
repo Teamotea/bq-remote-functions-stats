faeaa1e3bb6666e0f37f3e13676f9c5a1d9f8a3c
temote <i.temote315@gmail.com>
2022-07-24 16:40:20 +0900

    Add test function

--- statstests/prop.py
+++ statstests/prop.py
@@ -1,7 +1,28 @@
 from typing import Any
-from scipy import stats
+from dataclasses import dataclass
+from statsmodels.stats.proportion import proportions_ztest
 
 
-def test(calls: list[list[Any]]):
-    assert len(calls) == 1
-    pass
+@dataclass
+class SingleProportion:
+    successes: int
+    trials: int
+
+
+@dataclass
+class PropTestResult:
+    result: tuple[tuple[float, float], Any]
+
+
+def test(calls: list[list[Any]]) -> PropTestResult:
+    assert len(calls) == 2
+    p1 = SingleProportion(calls[0][0], calls[0][1])
+    p2 = SingleProportion(calls[1][0], calls[1][1])
+    count = [p1.successes, p2.successes]
+    nobs = [p1.trials, p2.trials]
+    stat, pval = proportions_ztest(count, nobs)
+    result = PropTestResult(
+        [stat, pval],
+        [None, None]
+    )
+    return result
